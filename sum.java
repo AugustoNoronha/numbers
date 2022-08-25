6d5fc66b032fc8df7082bd4b111c9ca07ecc16d4
AugustoNoronha <64671692+AugustoNoronha@users.noreply.github.com>
2022-08-24 22:18:24 -0300

    somatorio de dois numero em java

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class sum {
+    public static void main(String[] args) {
+        int sum = sumNumbers(10,20);
+        System.out.println(sum);
+
+    }
+
+    public static int sumNumbers(int n1, int n2){
+        return n1+n2;
+
+    }
+
+   
+}
